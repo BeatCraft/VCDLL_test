567cb792e3cfafb5a4d311ee9591857cf6dcae18
Masashi Takemoto <lesser@beatcraft.jp>
2020-12-03 20:01:48 +0900

    Update VCDLL.py

--- VCDLL.py
+++ VCDLL.py
@@ -345,10 +345,6 @@ class VidepCapture():
         #
         obj = self._dev_list[d_id][0];
         self._vcdll.Dev_Stop(obj);
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
         return 0 # success
 
     def terminate(self):
